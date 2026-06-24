a0ce1c55013e5b076946b1053b83f7bf0c3fdb67
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2026-06-24 09:05:00 -0300

    Update vite.config.ts

--- FrontEnd/sisconpp-app/vite.config.ts
+++ FrontEnd/sisconpp-app/vite.config.ts
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002', // Porta do seu backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [react()]
 });
